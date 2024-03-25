52c06062856022ef0893f8399e6ff2d54131f87b
uopsdod <uopsdod@gmail.com>
2024-03-24 23:35:55 -0700

    u

--- dynamodb_read_write/dynamodb_02_add_items.py
+++ dynamodb_read_write/dynamodb_02_add_items.py
@@ -5,6 +5,14 @@ region = 'us-east-2'
 session = botocore.session.get_session()
 dynamodb = session.create_client('dynamodb', region_name=region) # low-level client
 
+# DAX Cache (for learning purpose)
+import sys
+if len(sys.argv) > 1:
+    from amazondax import AmazonDaxClient
+    dax_cluster_endpoint = sys.argv[1]  # Assuming the DAX endpoint is the first argument
+    dynamodb = AmazonDaxClient(session, endpoints=[dax_cluster_endpoint], region_name=region)
+    print("DAX in use.")
+
 table_name = "game-python-001"
 
 # item01
