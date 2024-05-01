a977fd65a9d472823689d1e5d582163cb1e88a93
uopsdod <uopsdod@gmail.com>
2024-04-30 22:27:43 -0700

    u

--- lambda_provisioned_zip/lambda_function_with_db.py
+++ lambda_provisioned_zip/lambda_function_with_db.py
@@ -5,19 +5,18 @@ is_db_connection_built = False
 
 def lambda_handler(event, context):
     create_database_connection()
+    time.sleep(3)  # processing main logic
     return 'Hello from AWS Lambda using Python' + sys.version + '!\n'
 
 def create_database_connection():
     global is_db_connection_built  # Declare the variable as global
     if is_db_connection_built is False:
         print('DB connection is being built')
-        time.sleep(5)  # Delay for 120 seconds or 2 minutes
+        time.sleep(5)  # getting db connection
         is_db_connection_built = True
         print('DB connection built')
     else:
         print('DB connection already built')
-
-    time.sleep(3)  # Delay for 120 seconds or 2 minutes
 
 # static initilization 
 print('static initialization starts')
